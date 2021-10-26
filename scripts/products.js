f00f8d3ba894ec4e55d01a0c239791b2471e8442
Danicode0210 <alejandrad087@gmail.com>
2021-10-26 08:38:40 -0500

    Métodos GET y POST terminados

--- scripts/products.js
+++ scripts/products.js
@@ -1,34 +1,59 @@
 
+let serverData = 'http://localhost:4000/products';
 
 
-const getProducts = async (url) =>{
-
+/* Obtener la data y pintarla */
+const getProducts = async (url) => {
     let getProducts = document.getElementById('containerProducts')
-    getProducts.innerHTML='';
+    getProducts.innerHTML = '';
     const res = await fetch(url)
     const data = await res.json();
 
-    data.forEach( product => {
-        
-        const {description,category,price,image} = product;
+    data.forEach(product => {
+        const { description, category, price, image } = product;
         getProducts.innerHTML += `
-        <div class="container ">
-        <a href="#" class="enlace-detalle-mascota">
-            <div class="card bg-dark text-white gradiente">                
-                <img src="${image}" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                        <h5 class="card-title body2Bold">${description}</h5>
-                        <h5 class="card-title body2Bold">${category}</h5>
-                        <p class="card-text body2Regular">${price}</p>
-                </div>
+        <div class=" mt-3">
+        <div class="card" style="width: 18rem;">
+            <img src="${image}" class="card-img-top" alt="image-product">
+            <div class="card-body">
+                <h5 class="card-title">Categoría:${category}</h5>
+                <p class="card-text">${description}</p>
+                <p class="card-text">$${price}</p>
+                <a href="#" class="btn btn-primary">Añadir al carrito</a>
             </div>
-        </a>
+            </div>
     </div>
         `
-        
+
     });
-
-
 }
 
-getProducts('http://localhost:4005/products')
+getProducts(serverData)
+
+
+
+/* Agregar nuevo producto */
+
+let formulario = document.getElementById('form')
+formulario.addEventListener('submit',  async (e)=>{
+    /* Prevenir en evento */
+    e.preventDefault();
+    let description = document.getElementById('description').value;
+    let category = document.getElementById('category').value;
+    let price = document.getElementById('price').value;
+    let image = document.getElementById('image').value;
+
+    await fetch(serverData ,{
+        method:'POST',
+        body:JSON.stringify({
+            description,
+            category,
+            price,
+            image
+        }),
+
+        headers:{
+            "Content-type":"application/json; charset =UTF-8"
+        } 
+    })
+})
